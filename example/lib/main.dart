061c70a4e002d9575aa6df34a5c20105719fa51f
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2022-01-23 21:41:05 +0530

    added android example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -35,7 +35,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Row(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TfLinearGradientShader(
