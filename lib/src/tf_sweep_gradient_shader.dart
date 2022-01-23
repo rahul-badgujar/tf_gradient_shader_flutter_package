b419184d2e0209131aec79cec06774517f1a19ba
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2022-01-23 19:53:44 +0530

    added support sweep gradient shader

---
+++ lib/src/tf_sweep_gradient_shader.dart
@@ -0,0 +1,46 @@
+import 'dart:typed_data';
+
+import 'package:flutter/widgets.dart';
+import 'dart:ui' as ui;
+import '../tf_gradient_shader.dart';
+import 'dart:math' as math;
+
+class TfSweepGradientShader extends TfGradientShader {
+  const TfSweepGradientShader({
+    Key? key,
+    required Widget child,
+    required List<Color> colors,
+    required List<double> colorStops,
+    BlendMode blendMode = BlendMode.modulate,
+    TileMode tileMode = TileMode.clamp,
+    Float64List? float64list,
+    required this.center,
+    this.startAngle = 0.0,
+    this.endAngle = 2 * math.pi,
+  }) : super(
+          key: key,
+          child: child,
+          colors: colors,
+          colorStops: colorStops,
+          tileMode: tileMode,
+          blendMode: blendMode,
+          float64list: float64list,
+        );
+
+  final Offset center;
+  final double startAngle;
+  final double endAngle;
+
+  @override
+  Shader getShader() {
+    return ui.Gradient.sweep(
+      center,
+      colors,
+      colorStops,
+      tileMode,
+      startAngle,
+      endAngle,
+      float64list,
+    );
+  }
+}
--- lib/tf_gradient_shader.dart
+++ lib/tf_gradient_shader.dart
@@ -3,3 +3,4 @@ library tf_gradient_shader;
 export 'src/tf_gradient_shader_base.dart';
 export 'src/tf_linear_gradient_shader.dart';
 export 'src/tf_radial_gradient_shader.dart';
+export 'src/tf_sweep_gradient_shader.dart';
